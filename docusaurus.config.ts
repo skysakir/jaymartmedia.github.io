3463ab52d2e83ad9d4be8b542358b0841135fae7
JayMartMedia <Jay@JayMartMedia.com>
2024-11-16 17:40:20 -0500

    feat: add google tag manager

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,6 +51,9 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        googleTagManager: {
+          containerId: 'GTM-53X4MRZH',
+        },
       } satisfies Preset.Options,
     ],
   ],
